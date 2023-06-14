772d00d671e77f73abb59a7a2a04e48e722f5f07
healingdrawing <healingdrawing@gmail.com>
2023-06-14 10:09:32 +0300

    Prepare groupId reset to avoid filtering posts

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="nav-bar__links">
       <router-link to="/profile">Profile</router-link> |
-      <router-link to="/posts">Posts</router-link> |
+      <router-link to="/posts" @click="piniaManageData()">Posts</router-link> |
       <router-link to="/chats">Chats</router-link> |
       <router-link to="/groups">Groups</router-link> |
       <router-link to="/">Logout</router-link>
@@ -13,3 +13,14 @@
     <router-view/>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { useGroupStore } from '@/store/group';
+
+// when "posts" click happens, reset group id to -1 or 0, to prevent backend filtering of the posts to not show group only posts, but show all
+const groupStore = useGroupStore();
+function piniaManageData() {
+  alert('piniaManageData posts navbar');
+  groupStore.setGroupId(-1); //todo: implement on backend. Now posts will be not filtered by group id. But only filtered by date, fresh first
+}
+</script>
