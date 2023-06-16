fe8dc1d5e1eac5e0d37fe7fffa8eeb4170ff1eaf
healingdrawing <healingdrawing@gmail.com>
2023-06-16 15:07:55 +0300

    Extend todo

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -4,7 +4,7 @@
       <img src="../assets/logo.png" alt="Vue logo" />
     </div>
     <div class="nav-bar__links">
-      <!-- todo: add implementation to mark/hightlight "Profile" and "Chats" -->
+      <!-- todo: add implementation to mark/hightlight "Profile" and "Chats". Manage the "hasNewsBells" and "hasNewMessages" using backend data through the pinia store methods -->
       <router-link :class="{ highlighted: hasNewBells }" to="/profile">Profile</router-link> |
       <router-link to="/posts" @click="piniaManageData()">Posts</router-link> |
       <router-link :class="{ highlighted: hasNewMessages }" to="/chats">Chats</router-link> |
