2fe90675344cca6b6b6055ee5f7a9836ee1f261f
sagarishere <5121817+sagarishere@users.noreply.github.com>
2023-06-25 06:41:52 +0300

    preflight request was being sent from frontend: resolved

--- backend/main.go
+++ backend/main.go
@@ -33,7 +33,15 @@ func main() {
 
 	corsMiddleware := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
 			next.ServeHTTP(w, r)
 		})
 	}
