9cf7a99ae66724d97c128dbf13b605d081094ddb
healingdrawing <healingdrawing@gmail.com>
2023-07-03 13:21:45 +0300

    wip error. before refactoring to multirejection

--- backend/ws_approvals.go
+++ backend/ws_approvals.go
@@ -102,156 +102,168 @@ func wsGroupRequestAcceptHandler(conn *websocket.Conn, messageData map[string]in
 
 }
 
-// # groupRequestRejectHandler is the handler for rejecting a group request
+// wsGroupRequestRejectHandler is the handler for rejecting a group request
 //
-// @rparam {group_id int, member_email string}
-func groupRequestRejectHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	defer recovery(w)
+// @rparam {user_uuid string, group_id int, requester_email string}
+func wsGroupRequestRejectHandler(conn *websocket.Conn, messageData map[string]interface{}) {
+	defer wsRecover()
 
-	LoggedinUserID, err := getRequestSenderID(r)
+	uuid, ok := messageData["user_uuid"].(string)
+	if !ok {
+		log.Println("failed to get user_uuid from message data")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get user_uuid from message data"})
+		return
+	}
+
+	group_creator_id, err := getIDbyUUID(uuid)
 	if err != nil {
-		jsonResponse(w, http.StatusUnauthorized, err.Error())
+		log.Println("failed to get ID of the accept request sender", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get ID of the accept request sender"})
 		return
 	}
 
-	// incoming DTO GroupMember
-	data := GroupMemberRequest{}
-	// decode the request body into the DTO
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	err = decoder.Decode(&data)
+	group_id, ok := messageData["group_id"].(int)
+	if !ok {
+		log.Println("failed to get group_id from message data")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get group_id from message data"})
+		return
+	}
+
+	requester_email, ok := messageData["requester_email"].(string)
+	if !ok {
+		log.Println("failed to get requester_email from message data")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get requester_email from message data"})
+		return
+	}
+
+	requester_id, err := getIDbyEmail(requester_email)
 	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusBadRequest, "")
+		log.Println("failed to get ID of the request sender", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get ID of the request sender"})
 		return
 	}
 
-	memberID, err := getIDbyEmail(data.Email)
+	rows, err := statements["getGroup"].Query(group_id)
 	if err != nil {
-		jsonResponse(w, http.StatusBadRequest, "") // todo: fresh handling, in old version was just skipped
+		log.Println("getGroup query failed", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " getGroup query failed"})
 		return
 	}
-
-	rows, err := statements["getGroup"].Query(data.GroupID)
-	defer rows.Close() // todo: it says this should be after error checking, but it is only warning
-	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusInternalServerError, "getGroup query failed")
-		return
-	}
-	group := Group{}
-
-	for rows.Next() {
-		err = rows.Scan(&group.ID, &group.Name, &group.Description, &group.CreatorId, &group.CreationDate, &group.Privacy)
-		if err != nil {
-			log.Println(err.Error())
-			jsonResponse(w, http.StatusInternalServerError, "failed to scan group")
-			return
-		}
-	}
-
-	if group.CreatorId != LoggedinUserID {
-		jsonResponse(w, http.StatusUnauthorized, "you are not the creator of the group")
-		return
-	}
-
-	_, err = statements["removeGroupPendingMember"].Exec(data.GroupID, memberID)
-	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusInternalServerError, "removeGroupPendingMember query failed")
-		return
-	}
-
-	jsonResponse(w, http.StatusOK, "success: you rejected the group membership")
-	return
-}
-
-// # approveFollowerHandler is the handler for accepting a follower request
-//
-// @rparam {email string}
-func approveFollowerHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	defer recovery(w)
-
-	LoggedinUserID, err := getRequestSenderID(r)
-	if err != nil {
-		jsonResponse(w, http.StatusUnauthorized, err.Error())
-		return
-	}
-
-	// incoming DTO UserFollowerRequest
-	data := UserFollowerRequest{}
-	// decode the request body into the DTO
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	err = decoder.Decode(&data)
-	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusBadRequest, "")
-		return
-	}
-
-	fanID, err := getIDbyEmail(data.Email)
-	if err != nil {
-		jsonResponse(w, http.StatusBadRequest, "") // todo: fresh handling, in old version was just skipped
-		return
-	}
-
-	// check if the user has the follower in the followers_pending table
-	// use the getFollowersPending statement
-	rows, err := statements["getFollowersPending"].Query(LoggedinUserID)
 	defer rows.Close()
-	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusInternalServerError, "getFollowersPending query failed")
-		return
-	}
-	var followerID int
-	var followersPending []int
+
+	var group WS_GROUP_CHECK_DTO
+
 	for rows.Next() {
-		err = rows.Scan(&followerID)
+		err = rows.Scan(&group.Id, &group.Name, &group.Description, &group.Creator_id, &group.Created_at, &group.Privacy)
 		if err != nil {
-			log.Println(err.Error())
-			jsonResponse(w, http.StatusInternalServerError, "failed to scan followers pending")
+			log.Println("failed to scan group", err.Error())
+			wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " failed to scan group"})
 			return
-		}
-		followersPending = append(followersPending, followerID)
-	}
-	// check if the user is in the list of followersPending request
-	found := false
-	for _, follower := range followersPending {
-		if follower == fanID {
-			found = true
-			break
 		}
 	}
 
-	if !found {
-		jsonResponse(w, http.StatusBadRequest, "the user is not in the list of followers pending")
+	if group.Creator_id != group_creator_id {
+		log.Println("request sender is not the creator of the group")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnauthorized) + " request sender is not the creator of the group"})
 		return
 	}
-	// add the follower to the followers table
-	_, err = statements["addFollower"].Exec(LoggedinUserID, fanID)
+
+	_, err = statements["removeGroupPendingMember"].Exec(group_id, requester_id)
 	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusInternalServerError, "addFollower query failed")
+		log.Println("removeGroupPendingMember query failed", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " removeGroupPendingMember query failed"})
 		return
 	}
-	// remove it from the followers_pending table
-	_, err = statements["removeFollowerPending"].Exec(LoggedinUserID, fanID)
-	if err != nil {
-		log.Println(err.Error())
-		jsonResponse(w, http.StatusInternalServerError, "removeFollowerPending query failed")
-		return // todo: CHECK IT! Was added , before it was just continue of the flow, to next line
-	}
-	jsonResponse(w, http.StatusOK, "success: you accepted the follow request")
-	return
+
+	wsSendSuccess(WS_SUCCESS_RESPONSE_DTO{fmt.Sprint(http.StatusOK) + "success: you rejected the group membership"})
+
 }
 
 // swRejectFollowerHandler is the handler for rejecting a follower request
 //
 // @rparam {email string}
+func wsApproveFollowerHandler(conn *websocket.Conn, messageData map[string]interface{}) {
+	defer wsRecover()
+
+	uuid, ok := messageData["user_uuid"].(string)
+	if !ok {
+		log.Println("failed to get user_uuid from messageData")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get user_uuid from messageData"})
+		return
+	}
+
+	idol_user_id, err := getIDbyUUID(uuid)
+	if err != nil {
+		log.Println("failed to get ID of the request sender", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get ID of the request sender"})
+		return
+	}
+
+	target_email, ok := messageData["target_email"].(string)
+	if !ok {
+		log.Println("failed to get target_email from messageData")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get target_email from messageData"})
+		return
+	}
+
+	fan_user_id, err := getIDbyEmail(target_email)
+	if err != nil {
+		log.Println("failed to get ID of the follow request creator", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusUnprocessableEntity) + " failed to get ID of the follow request creator"})
+		return
+	}
+
+	// check if the user has the follower in the followers_pending table
+	// use the getFollowersPending statement
+	rows, err := statements["getFollowersPending"].Query(idol_user_id)
+	defer rows.Close()
+	if err != nil {
+		log.Println("getFollowersPending query failed", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " getFollowersPending query failed"})
+		return
+	}
+	var follower_id int
+	var followers_pending map[int]int
+	for rows.Next() {
+		err = rows.Scan(&follower_id)
+		if err != nil {
+			log.Println("failed to scan followers pending", err.Error())
+			wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " failed to scan followers pending"})
+			return
+		}
+		followers_pending[follower_id] = follower_id
+	}
+	// check if the user is in the list of followersPending request
+	_, ok = followers_pending[fan_user_id]
+
+	if !ok {
+		log.Println("the user is not in the list of followers pending")
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusBadRequest) + " the user is not in the list of followers pending"})
+		return
+	}
+
+	// add the follower to the followers table
+	_, err = statements["addFollower"].Exec(idol_user_id, fan_user_id)
+	if err != nil {
+		log.Println("addFollower query failed", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " addFollower query failed"})
+		return
+	}
+
+	// remove it from the followers_pending table
+	_, err = statements["removeFollowerPending"].Exec(idol_user_id, fan_user_id)
+	if err != nil {
+		log.Println("removeFollowerPending query failed", err.Error())
+		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " removeFollowerPending query failed"})
+		return
+	}
+	wsSendSuccess(WS_SUCCESS_RESPONSE_DTO{fmt.Sprint(http.StatusOK) + "Success: you rejected the follow request"})
+	return
+}
+
+// swRejectFollowerHandler is the handler for rejecting a follower request
+//
+// @rparam invited_emails (string space separated}
 func wsRejectFollowerHandler(conn *websocket.Conn, messageData map[string]interface{}) {
 	defer wsRecover()
 
@@ -319,6 +331,7 @@ func wsRejectFollowerHandler(conn *websocket.Conn, messageData map[string]interf
 		wsSendError(WS_ERROR_RESPONSE_DTO{fmt.Sprint(http.StatusInternalServerError) + " removeFollowerPending query failed"})
 		return
 	}
+
 	wsSendSuccess(WS_SUCCESS_RESPONSE_DTO{fmt.Sprint(http.StatusOK) + "Success: you rejected the follow request"})
 	return
 }
