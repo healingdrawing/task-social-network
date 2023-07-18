6735ab1dbb1beb3450ca7fbf1642ff5932922aab
healingdrawing <kyznector@gmail.com>
2023-07-18 20:13:22 +0300

    bug founded, loosing of data after refresh page.  will fail audit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -63,85 +63,85 @@ nav {
 }
 
 /* styles */
-:root {
-  --gradient: linear-gradient(to right, #b74cc0, #bd08e3 30%, #bd08e3 70%, #b74cc0);
-  --goldenGradient: linear-gradient(to bottom, #fde056, #ffffff 30%, #fec722);
-  --yellowPink: linear-gradient(to bottom, #fde056, #ffffff 30%, #b74cc0);
-}
+// :root {
+//   --gradient: linear-gradient(to right, #b74cc0, #bd08e3 30%, #bd08e3 70%, #b74cc0);
+//   --goldenGradient: linear-gradient(to bottom, #fde056, #ffffff 30%, #fec722);
+//   --yellowPink: linear-gradient(to bottom, #fde056, #ffffff 30%, #b74cc0);
+// }
 
 
-body {
-  background-color: #bd08e3 !important;
-  background: var(--gradient);
-  background-size: 100% 100vh;
-  color: white;
-}
+// body {
+//   background-color: #bd08e3 !important;
+//   background: var(--gradient);
+//   background-size: 100% 100vh;
+//   color: white;
+// }
 
-.label_file_upload input[type="file"] {
-  /* Hide the default file input */
-  display: none;
-}
+// .label_file_upload input[type="file"] {
+//   /* Hide the default file input */
+//   display: none;
+// }
 
-.label_file_upload,
-button {
-  background: var(--goldenGradient);
-  color: #b74cc0;
-  border: 2px solid white;
-  padding: 24px;
-  margin: 24px;
-  text-decoration: none;
-  border-radius: 100%;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  display: inline-block;
-  cursor: pointer;
-}
+// .label_file_upload,
+// button {
+//   background: var(--goldenGradient);
+//   color: #b74cc0;
+//   border: 2px solid white;
+//   padding: 24px;
+//   margin: 24px;
+//   text-decoration: none;
+//   border-radius: 100%;
+//   word-wrap: break-word;
+//   overflow-wrap: break-word;
+//   display: inline-block;
+//   cursor: pointer;
+// }
 
-a {
-  text-decoration: none;
-  color: #b74cc0;
-  cursor: pointer;
-}
+// a {
+//   text-decoration: none;
+//   color: #b74cc0;
+//   cursor: pointer;
+// }
 
-a.router-link-exact-active {
-  color: #003FAE;
-}
+// a.router-link-exact-active {
+//   color: #003FAE;
+// }
 
-a:hover {
-  color: #511162;
-  font-size: 110%;
-}
+// a:hover {
+//   color: #511162;
+//   font-size: 110%;
+// }
 
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  text-shadow:
-    #511162 0px 0px 10px;
-  color: #b74cc0;
-}
+// h1,
+// h2,
+// h3,
+// h4,
+// h5,
+// h6 {
+//   text-shadow:
+//     #511162 0px 0px 10px;
+//   color: #b74cc0;
+// }
 
-.single_div_box {
-  border-radius: 24px;
-  border-left: 2px solid #fde056;
-  border-right: 2px solid #fec722;
+// .single_div_box {
+//   border-radius: 24px;
+//   border-left: 2px solid #fde056;
+//   border-right: 2px solid #fec722;
 
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  padding: 24px;
-}
+//   word-wrap: break-word;
+//   overflow-wrap: break-word;
+//   padding: 24px;
+// }
 
-.router_link_box {
-  background: var(--goldenGradient);
-  border: 2px solid white;
-  padding: 24px;
-  margin-bottom: -24px;
-  text-decoration: none;
-  border-radius: 100%;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  display: inline-block;
-}
+// .router_link_box {
+//   background: var(--goldenGradient);
+//   border: 2px solid white;
+//   padding: 24px;
+//   margin-bottom: -24px;
+//   text-decoration: none;
+//   border-radius: 100%;
+//   word-wrap: break-word;
+//   overflow-wrap: break-word;
+//   display: inline-block;
+// }
 </style>
