3a955a917da571c79b4d61466344e329d14be35e
healingdrawing <kyznector@gmail.com>
2023-07-20 07:12:05 +0300

    before clear css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -64,14 +64,32 @@ nav {
 
 /* styles */
 :root {
+  --yellow_dark: #fec722;
+  --yellow_light: #fde056;
+  --pink_dark: #511162;
   --gradient: linear-gradient(to right, #b74cc0, #bd08e3 30%, #bd08e3 70%, #b74cc0);
-  --goldenGradient: linear-gradient(to bottom, #fde056, #ffffff 30%, #fec722);
-  --yellowPink: linear-gradient(to bottom, #fde056, #ffffff 30%, #b74cc0);
-  --whiteGlassHorizontal: linear-gradient(to right,
+  --g_horizontal_white_glass: linear-gradient(to right,
       hsla(0, 0%, 100%, 0.01),
       #ffffff00 30%,
       #ffffff00 70%,
       hsla(0, 0%, 100%, 0.10));
+  --g_button_white_glass: linear-gradient(to right,
+      hsla(0, 0%, 100%, 0.10),
+      #ffffff00 30%,
+      #ffffff00 70%,
+      hsla(0, 0%, 100%, 0.10));
+
+  --g_button_hover_white_glass: linear-gradient(to right,
+      hsla(0, 0%, 100%, 0.30),
+      #ffffff00 30%,
+      #ffffff00 70%,
+      hsla(0, 0%, 100%, 0.30));
+
+  --g_active_router_link: linear-gradient(to right,
+      white,
+      #511162 20px,
+      #511162 calc(100% - 20px),
+      white);
 }
 
 
@@ -82,16 +100,19 @@ body {
   color: white;
 }
 
+* {
+  color: white; // to fix select multiple different color, which ignores body, facepalm
+}
+
 // fucking headache
 
 .users_list_with_scroll {
-  background: var(--whiteGlassHorizontal);
+  background: var(--g_horizontal_white_glass);
   height: 100px;
   overflow-y: scroll;
   overflow-x: hidden;
   scrollbar-color: white #fde056 !important;
 }
-
 
 .users_list_with_scroll::-webkit-scrollbar {
   width: 2px;
@@ -103,6 +124,22 @@ body {
 
 .users_list_with_scroll::-webkit-scrollbar-track {
   background-color: #fde056;
+}
+
+textarea {
+  scrollbar-color: white none !important;
+}
+
+textarea::-webkit-scrollbar {
+  width: 2px;
+}
+
+textarea::-webkit-scrollbar-thumb {
+  background-color: white;
+}
+
+textarea::-webkit-scrollbar-track {
+  background-color: none;
 }
 
 // end of fucking headache
@@ -117,32 +154,41 @@ body {
 
 .label_file_upload,
 button {
-  background: var(--goldenGradient);
-  color: #b74cc0;
-  border: 2px solid white;
+  background: var(--g_button_white_glass);
+  border-style: solid;
+  border-width: 0 2px;
+  border-color: white;
+  border-radius: 24px;
+  color: var(--yellow_light);
   padding: 24px;
   margin: 24px;
-  text-decoration: none;
-  border-radius: 100%;
   word-wrap: break-word;
   overflow-wrap: break-word;
   display: inline-block;
   cursor: pointer;
+
+}
+
+.label_file_upload:hover,
+button:hover {
+  background: var(--g_button_hover_white_glass);
+  text-shadow: 0px 0px 2px white;
 }
 
 a {
   text-decoration: none;
-  color: #b74cc0;
+  color: var(--yellow_light);
   cursor: pointer;
 }
 
 a.router-link-exact-active {
-  color: #003FAE;
+  background: var(--g_active_router_link);
+  // #511162
+  // #003FAE
 }
 
 a:hover {
-  color: #511162;
-  font-size: 110%;
+  text-shadow: 0px 0px 2px white;
 }
 
 h1,
@@ -153,7 +199,6 @@ h5,
 h6 {
   text-shadow: #511162 0px 0px 10px;
   color: hsl(295, 71%, 63%);
-  // color: #b74cc0;
 }
 
 .single_div_box {
@@ -167,14 +212,59 @@ h6 {
 }
 
 .router_link_box {
-  background: var(--goldenGradient);
-  border: 2px solid white;
+  background: var(--g_button_white_glass);
+  border-left: 2px solid white;
+  border-right: 2px solid white;
+  border-radius: 24px;
   padding: 24px;
   margin-bottom: -24px;
   text-decoration: none;
-  border-radius: 100%;
   word-wrap: break-word;
   overflow-wrap: break-word;
   display: inline-block;
+}
+
+.router_link_box:hover {
+  background: var(--g_button_hover_white_glass);
+}
+
+input {
+  background: var(--g_horizontal_white_glass);
+  border-style: solid;
+  border-width: 0 2px;
+  border-color: var(--yellow_light);
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+}
+
+input:focus {
+  outline: none;
+  border-color: white;
+  background: var(--pink_dark);
+}
+
+select {
+  overflow: hidden;
+  border: none;
+}
+
+select:focus {
+  outline: none;
+  background: var(--pink_dark);
+}
+
+textarea {
+  background: var(--g_horizontal_white_glass);
+  border-style: solid;
+  border-width: 0 2px;
+  border-color: var(--yellow_light);
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+}
+
+textarea:focus {
+  outline: none;
+  border-color: white;
+  background: var(--pink_dark);
 }
 </style>
